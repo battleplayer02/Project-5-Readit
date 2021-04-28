70d12f44d4cbf310ad52f5f1c98b8e4d7011fe44
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-04-29 01:55:22 +0530

    f

--- ormconfig.js
+++ ormconfig.js
@@ -7,7 +7,7 @@ module.exports = {
   // username: process.env.DB_USERNAME,
   // password: process.env.DB_PASSWORD,
   // database: process.env.DB_DATABASE,
-  url:'postgres://yduualjlyktgue:bfd82547362fc9c2ab6e4a1a20e1965994735f3dfca14418e25c995ad591ef88@ec2-18-215-111-67.compute-1.amazonaws.com:5432/db2cgu882qfj8k',
+  url: 'postgres://nybtxgft:lD_e6YCYWguoj6ANP4XtXLDSfQ6Oahd1@queenie.db.elephantsql.com:5432/nybtxgft ',
   synchronize: true,
   logging: process.env.NODE_ENV === 'development',
   entities: [rootDir + '/entities/**/*{.ts,.js}'],
