f84a329f96a1361e02ace2302a070caa5cb5982d
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-04-29 01:46:02 +0530

    f

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,12 @@ const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'build'
 
 module.exports = {
   type: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DATABASE,
+  url:'postgres://yduualjlyktgue:bfd82547362fc9c2ab6e4a1a20e1965994735f3dfca14418e25c995ad591ef88@ec2-18-215-111-67.compute-1.amazonaws.com:5432/db2cgu882qfj8k',
   synchronize: true,
   logging: process.env.NODE_ENV === 'development',
   entities: [rootDir + '/entities/**/*{.ts,.js}'],
